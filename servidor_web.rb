7fa3e61287da01b39eb10e71064aa6ee51421718
Cassio Almeida <jcassiocosta@gmail.com>
2016-07-20 13:18:35 -0300

    removido código desnecessário

--- servidor_web.rb
+++ servidor_web.rb
@@ -65,15 +65,3 @@ class ServidorWeb
   end
 
 end
-
-# servidor = TCPServer.new('localhost', '2323')
-
-# loop do 
-#   socket = servidor.accept
-#   requisicao = socket.gets
-#   STDERR.puts requisicao
-
-#   Thread.start(socket, requisicao) do |sessao, requisicao|
-#     ServidorWeb.new(socket, requisicao).servir()
-#   end
-# end
