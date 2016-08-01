4a62eaa3eddd5b27549721c7916c4575cc8e1654
Cassio Almeida <jcassiocosta@gmail.com>
2016-08-01 16:12:18 -0300

    substituir STDERR.puts por puts

--- principal.rb
+++ principal.rb
@@ -6,14 +6,14 @@ PORTA = 2323
 
 servidor = TCPServer.new(HOST, PORTA)
 
-STDERR.puts "Servidor rodando em http://#{HOST}:#{PORTA}"
+puts "Servidor rodando em http://#{HOST}:#{PORTA}"
 
 loop do 
   socket = servidor.accept
   requisicao = socket.gets
 
-  STDERR.puts requisicao
-  STDERR.puts Time.now
+  puts requisicao
+  puts Time.now
 
   Thread.start(socket, requisicao) do |s, r|
     ServidorWeb.new(s, r).servir()
