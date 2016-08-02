6b6564df638d7552a74c6c2c171c6b156bc7a634
Cassio Almeida <jcassiocosta@gmail.com>
2016-08-02 11:18:31 -0300

    adicionado alguns comentários

--- principal.rb
+++ principal.rb
@@ -1,21 +1,24 @@
-require 'socket'
+require 'socket' # Faz o 'import' dos módulos
 require './servidor_web.rb'
 
 HOST = 'localhost'
 PORTA = 2323
 
+# Cria um objeto TCPServer, que implementa sockets da classe Socket
 servidor = TCPServer.new(HOST, PORTA)
 
+# Apenas exibe no terminal
 puts "Servidor rodando em http://#{HOST}:#{PORTA}"
 
-loop do 
-  socket = servidor.accept
-  requisicao = socket.gets
+loop do # loop infinito, ou seja, o servidor vai ficar rodando até o processor ser interrompido
+  socket = servidor.accept # Fica esperando por uma requisição e retorna um objeto do tipo socket
+  requisicao = socket.gets # Retorna as linha de requisição
 
-  puts requisicao
-  puts Time.now
+  puts requisicao # Exibe no terminal a requisição feita
+  puts Time.now # Exibe no terminal a data e hora atual
 
+  # Criar thread em ruby(em modo usuário)
   Thread.start(socket, requisicao) do |s, r|
-    ServidorWeb.new(s, r).servir()
+    ServidorWeb.new(s, r).servir # Instancia um objeto e chama o método servir()
   end
 end
--- servidor_web.rb
+++ servidor_web.rb
@@ -2,6 +2,7 @@ require 'uri'
 
 class ServidorWeb
 
+  # Construtor da classe. Recebe como parâmetro o socket e a requisição
   def initialize(socket, requisicao)
     @socket = socket
     @requisicao = requisicao
@@ -9,25 +10,29 @@ class ServidorWeb
 
   RAIZ_WEB = './publico'
 
+  # Hash que armazena os tipo de arquivos suportados pelo servidor
   TIPOS_CONTEUDO = {
     'html' => 'text/html',
-    'txt' => 'text/plain',
-    'png' => 'image/png',
-    'jpg' =>'image/jpeg'
+    'txt' =>  'text/plain',
+    'png' =>  'image/png',
+    'jpg' =>  'image/jpeg'
   }
 
-  TIPO_CONTEUDO_PADRAO = 'application/actet-stream'
+  TIPO_CONTEUDO_PADRAO = 'application/actet-stream' # Tipo binário
 
+  # Retorna a extensão do arquivo sem o '.', exemplo: obter_tipo_conteudo('foto.jpg') => 'jpg'
   def obter_tipo_conteudo(caminho)
     extensao = File.extname(caminho).split(".")[1]
     TIPOS_CONTEUDO.fetch(extensao, TIPO_CONTEUDO_PADRAO)
   end
 
+  # Retorno caminho completo do arquivo requisitado com a raiz do servidor, exemplo: ./publico/index.html
   def arquivo_requisitado(requisicao)
     uri_requisitada = requisicao.split(" ")[1]
     caminho = URI.unescape(URI(uri_requisitada).path)
     caminho_limpo = []
 
+    # Mecanismo de seguraça para impedir acessar diretório acima do diretório público
     partes = caminho.split('/')
 
     partes.each do |parte| 
@@ -37,23 +42,24 @@ class ServidorWeb
     File.join(RAIZ_WEB, caminho_limpo)
   end
 
+  # Método principal que faz toda a magia
   def servir
     @caminho = arquivo_requisitado(@requisicao) 
     @caminho = File.join(@caminho, 'index.html') if File.directory?(@caminho)
 
     if File.exists?(@caminho) && !File.directory?(@caminho)
-      File.open(@caminho, "rb") do |arquivo|
-        @socket.print "HTTP/1.0 200 OK\r\n"+
+      File.open(@caminho, "rb") do |arquivo| # Abre o arquivo @caminho e joga para variável arquivo(aberto)
+        @socket.print "HTTP/1.0 200 OK\r\n"+ # Método print escreve no socket
                      "Content-Type: #{obter_tipo_conteudo(arquivo)}; charset=utf-8\r\n"+
                      "Content-Length: #{arquivo.size}\r\n"+
                      "Connection: close\r\n"    
 
         @socket.print "\r\n"
-        IO.copy_stream(arquivo, @socket)
-        arquivo.close                   
+        IO.copy_stream(arquivo, @socket) # Copia os bytes do arquivo no socket
+        arquivo.close # Fecha arquivo              
       end
-    else
-      mensagem = "Arquivo não encontrado!\n"
+    else # Caso não exista o arquivo retorna 404
+      mensagem = "Arquivo não encontrado!(404 Not Found)\n"
       @socket.print "HTTP/1.1 404 Not Found\r\n"+
                    "Content-Type: text/plain; charset=utf-8\r\n"+
                    "Content-Length: #{mensagem.size}\r\n"
@@ -62,7 +68,7 @@ class ServidorWeb
       @socket.print "\r\n"
       @socket.print mensagem
     end
-    @socket.close
+    @socket.close # Fecha o socket
   end
 
 end
